48b72eb609eb4736b4852e3c038bcbbf817ae7ef
iheb-dev <53858411+iheb-dev@users.noreply.github.com>
2021-04-18 20:17:35 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/service/CommandeService.java
@@ -0,0 +1,18 @@
+package com.example.demo.service;
+
+import com.example.demo.Dao.repo.CommandeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+
+
+@Service
+public class CommandeService {
+    @Autowired
+    CommandeRepo commandeRepo;
+
+
+
+
+}
